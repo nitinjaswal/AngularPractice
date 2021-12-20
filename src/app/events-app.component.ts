6917ca04f1c9db146cc1e25eb3ec8d796334a215
nitin <nitinjas.chd@gmail.com>
2021-12-20 23:39:08 +0530

    Created events-list component

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: 'Hello world'
+  template: '<app-events-list></app-events-list>'
 })
 export class EventsAppComponent {
-  title = 'ClientApp';
+  
 }
